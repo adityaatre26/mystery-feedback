763c473418807988ae1627afc936b1c719eb283d
adityaatre26 <aditya.atre26@gmail.com>
2025-01-25 20:34:01 +0530

    Defined basic schemas for the project

---
+++ src/models/User.ts
@@ -0,0 +1,72 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+// The angular brackets are used to specify the interface it should follow as defined before
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string; // This code will be sent to the user's email for verification
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
+  isAcceptingMessage: boolean;
+  message: Message[]; // This is an array of messages received by the user
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/.+\@.+\..+/, "Please enter a valid email"], // This is a regex pattern to validate the email
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  verifyCode: {
+    type: String,
+    required: true,
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: true,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+  message: [MessageSchema],
+});
+
+// When exporting the first condition is for the case when the model is already defined
+// The second condition is for the case when the model is not defined
+const UserModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model<User>("User", UserSchema);
---
+++ src/schemas/readme.md
@@ -0,0 +1,4 @@
+This folder is made for the purpose of validation.
+It contains schemas for different stages which the user is going to go through such as signUp, signIn, etc.
+These schemas are defined just for validation while these stages are being completed.
+This validation is done using Zod.
